8c5a07346c5c2cd360149d28ec24b30bb77735a6
bhosle abhishek <abhishek.bhosle2001@gmail.com>
2024-07-06 10:22:08 +0530

    All file added

---
+++ math.js
@@ -0,0 +1,15 @@
+const sum =(a,b) => a + b;
+const multi =(a,b) => a * b;
+const sub =(a,b) => a - b;
+const PI = 3.14;
+
+
+let obj = {
+   sum: sum,
+   multi: multi,
+   sub: sub,
+   PI: PI,
+
+};
+
+module.export =obj;
---
+++ nodedemo.js
@@ -0,0 +1,13 @@
+// let args=process.argv;
+
+// for (let i=2; i<args.length; i++) {
+//     console.log("Hi My name is ",args[i]);
+// }
+
+// console.log(process.argv);
+
+
+
+const exa = require("./math")
+
+console.log(exa);
